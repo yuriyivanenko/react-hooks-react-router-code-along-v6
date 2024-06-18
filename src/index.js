4c085e64769f1982c416f53da8ec55924b2920a4
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-06-18 10:24:28 -0400

    Test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import routes from './routes'
 
 const router = createBrowserRouter(routes)
+//
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<RouterProvider router={router} />)
